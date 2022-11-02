fcc001a368f6dbf3e9fffe43a744d4624f14e7d4
Alex Loren <lorenalexm@gmail.com>
2022-11-02 13:58:35 -0700

    Initial Commit

---
+++ whatToWatch/whatToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatToWatchApp.swift
+//  whatToWatch
+//
+//  Created by Alex Loren on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct whatToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
